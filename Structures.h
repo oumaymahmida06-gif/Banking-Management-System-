f01b0b07abd3453f44edf4607937100e2e650662
Yassine Hannachi <hannachiyassine21@gmail.com>
2025-11-16 23:19:53 +0100

    Add files via upload

---
+++ Structures.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+struct Node {
+	int data;
+	Node* next;
+};
+struct List {
+	Node* head;
+	int size;
+};
+
+//We will add the additional structures in here
+
+#endif
