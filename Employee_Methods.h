fd07b97b51429da4bf59fd868a3e05c3ed7e7ad7
Yassine Hannachi <hannachiyassine21@gmail.com>
2025-11-22 16:49:22 +0100

    Add verification and display functions to Employee_Methods

--- Employee_Methods.h
+++ Employee_Methods.h
@@ -5,10 +5,21 @@
 
 void AddEmployee(Employee* employees, int& EmployeeCount, int& capacity);
 void NewArray(Employee*& employees, int& capacity, int EmployeeCount);
+bool Verify_Id(const int& ID);
+bool Verify_Salary(const double& Salary);
+bool Verify_Bank_Branch(const int& Bank_Branch);
+bool Verify_Address(const string& Address);
+bool Verify_Name(const string& Name);
+bool Verify_Last_Name(const string& Last_Name);
+bool Verify_Hire_Date(const string& date);
 int FindEmployee(Employee* employees, int ID, int EmployeeCount);
 void DeleteEmployee(Employee* employees, int& EmployeeCount);
 void ModifyEmployee(Employee* employees, int EmployeeCount);
+void PrintEmployee(const Employee& e);
 void DisplayEmployees(Employee* employees, int EmployeeCount);
 void DisplayEmployeesSortedByAlphabeticalOrder(Employee* employees, int EmployeeCount);
+void DisplayEmployeesGroupedByBankBranch(Employee* employees, int EmployeeCount);
+void DisplayEarliestAndRecentEmployees(Employee* employees, int EmployeeCount);
+string Reformulate(string hiredate);
 
 #endif
