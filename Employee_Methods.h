9b5f49b67fad5e45c4fcd78a88629de4d6bb6a26
Yassine Hannachi <hannachiyassine21@gmail.com>
2025-12-03 17:29:38 +0100

    Update Employee_Methods.h

--- Employee_Methods.h
+++ Employee_Methods.h
@@ -3,10 +3,18 @@
 #define EMPLOYEE_METHODS_H
 #include "Employee.h"
 #include "Customer.h"
+#include "SinglyLinkedList.h" 
+#include "QueueMeths.h"
 
+//------------------------ARRAY--------------------------
+void PrintEmployee(const Employee& e);
+void DisplayEmployees(Employee* employees, int EmployeeCount);
+int FindEmployee(Employee* employees, int ID, int EmployeeCount);
 
-void AddEmployee(Employee* employees, int& EmployeeCount, int& capacity);
+//DYNAMIC ALLOCATION:
 void NewArray(Employee*& employees, int& capacity, int EmployeeCount);
+
+//------------------------VERIFICATION--------------------------
 bool Verify_Id(const int& ID);
 bool Verify_Salary(const double& Salary);
 bool Verify_Bank_Branch(const int& Bank_Branch);
@@ -15,25 +23,21 @@ bool Verify_Name(const string& Name);
 bool Verify_Last_Name(const string& Last_Name);
 bool Verify_Hire_Date(const string& date);
 
-int FindEmployee(Employee* employees, int ID, int EmployeeCount);
+//------------------------EMPLOYEE INTERFACE FUNCTIONS--------------------------
+
+void AddEmployee(Employee* employees, int& EmployeeCount, int& capacity);
 void DeleteEmployee(Employee* employees, int& EmployeeCount);
 void ModifyEmployee(Employee* employees, int EmployeeCount);
-void PrintEmployee(const Employee& e);
 
-void DisplayEmployees(Employee* employees, int EmployeeCount);
 void DisplayEmployeesSortedByAlphabeticalOrder(Employee* employees, int EmployeeCount);
 void DisplayEmployeesGroupedByBankBranch(Employee* employees, int EmployeeCount);
 void DisplayEarliestAndRecentEmployees(Employee* employees, int EmployeeCount);
 string Reformulate(string hiredate);
-
-
 void Delete_Closed_Accounts_And_Store_Them(customer*& customers, int& CustomerCount, customer*& archived, int& ArchivedCount, int& ArchivedCapacity);
 void Display_Archived_Accounts(customer* archived, int ArchivedCount);
 void Display_List_Of_Loans_For_Specific_Customer(customer* customers, int CustomerCount);
-void employee_interface(Employee*& employees, int& EmployeeCount, int& capacity, customer*& customers, int& CustomerCount, int& Customer_Capacity, customer*& archived, int& ArchivedCount, int& Archived_Capacity);
+
+//------------------------INTERFACE FUNTION--------------------------
+void employee_interface(Employee*& employees, int& EmployeeCount, int& capacity, customer*& customers, int& CustomerCount, int& Customer_Capacity, customer*& archived, int& ArchivedCount, int& Archived_Capacity, CompletedLoanList& CompletedLoansList, Queue* Q);
 
 #endif
-
-
-
-
