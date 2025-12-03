f5626068ca592c8b31c6bbdae1c490cf59cc026a
Yassine Hannachi <hannachiyassine21@gmail.com>
2025-12-03 17:32:07 +0100

    Create TransactionMeths.h

---
+++ TransactionMeths.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef TRANSACTIONMETHS_H
+#define TRANSACTIONMETHS_H
+#include "Stack.h"
+
+Stack* CreateStack();
+void DestroyStack(Stack* S);
+void DisplayStack(const Stack& S);
+bool IsEmpty(const Stack& S);
+bool IsFull(const Stack& S);
+int StackSize(const Stack& S);
+int Push(Stack* S, transaction e);
+transaction Pop(Stack* S);
+transaction Top(const Stack& S);
+
+#endif
