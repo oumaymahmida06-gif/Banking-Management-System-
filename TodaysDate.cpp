49c7320e23d020d402b4d9679ff23f9b0c8f8327
Yassine Hannachi <hannachiyassine21@gmail.com>
2025-12-07 17:21:13 +0100

    Update TodaysDate.cpp

--- TodaysDate.cpp
+++ TodaysDate.cpp
@@ -8,16 +8,15 @@ string GetTodayDate() {
     tm tm_info;
 
 #ifdef _WIN32
-    localtime_s(&tm_info, &now);   // MSVC secure API
+    localtime_s(&tm_info, &now);
 #else
-    localtime_r(&now, &tm_info);   // POSIX reentrant API
+    localtime_r(&now, &tm_info); 
 #endif
 
     int day = tm_info.tm_mday;
     int month = 1 + tm_info.tm_mon;
     int year = 1900 + tm_info.tm_year;
 
-    // Format with leading zeros: DD-MM-YYYY
     std::ostringstream oss;
     oss << std::setfill('0') << std::setw(2) << day << "-"
         << std::setw(2) << month << "-" << year;
