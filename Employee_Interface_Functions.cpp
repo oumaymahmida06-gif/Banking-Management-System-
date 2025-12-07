2b3c71514c81ce1b69a5536ae46bab33dc6ce04c
Yassine Hannachi <hannachiyassine21@gmail.com>
2025-12-07 17:19:16 +0100

    Update Employee_Interface_Functions.cpp

--- Employee_Interface_Functions.cpp
+++ Employee_Interface_Functions.cpp
@@ -8,6 +8,7 @@
 #include "DailyTransactionsLogListMeths.h"
 #include "StatisticsMeths.h"
 #include "TodaysDate.h"
+#include <limits>
 using namespace std;
 
 //------------------------ARRAY--------------------------
@@ -57,7 +58,7 @@ void NewArray(Employee*& employees, int& capacity, int EmployeeCount) {
 //------------------------VERIFICATION--------------------------
 
 bool Verify_Id(const int& ID) {
-	if ((ID > 9999) || (ID<999)) {
+	if ((ID > 9999) || (ID < 999)) {
 		cout << "The ID must be a positive number containing 4 digits.\n";
 		return false;
 	}
@@ -162,50 +163,68 @@ void AddEmployee(Employee* employees, int& EmployeeCount, int& capacity) {
 		NewArray(employees, capacity, EmployeeCount);
 	}
 	Employee e;
-	int ID;
 	do {
 		cout << "Enter the ID of the employee:" << endl;
-		cin >> ID;
-	} while (!Verify_Id(ID));
-	e.id = ID;
-	cin.ignore();
-	string Name;
+		cin >> e.id;
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter numbers only.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+
+	} while (!Verify_Id(e.id));
+
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
 	do {
 		cout << "Enter the Name of the employee: ";
-		getline(cin, Name);
-	} while (!Verify_Name(Name));
-	e.Name = Name;
-	string Last_Name;
+		getline(cin, e.Name);
+	} while (!Verify_Name(e.Name));
+
 	do {
 		cout << "Enter the Last Name of the employee: ";
-		getline(cin, Last_Name);
-	} while (!Verify_Last_Name(Last_Name));
-	e.Last_Name = Last_Name;
-	string Address;
+		getline(cin, e.Last_Name);
+	} while (!Verify_Last_Name(e.Last_Name));
+
 	do {
 		cout << "Enter the Adress of the employee: ";
-		getline(cin, Address);
-	} while (!Verify_Address(Address));
-	e.Address = Address;
-	double salary;
+		getline(cin, e.Address);
+	} while (!Verify_Address(e.Address));
+
 	do {
 		cout << "Enter the Salary of the employee:" << endl;
-		cin >> salary;
-	} while (!Verify_Salary(salary));
-	e.Salary = salary;
-	cin.ignore();
-	string date;
+		cin >> e.Salary;
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter a valid number.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+
+	} while (!Verify_Salary(e.Salary));
+
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
 	do {
 		cout << "Enter the Hire Date of the employee: ";
-		getline(cin, date);
-	} while (!Verify_Hire_Date(date));
-	e.Hire_Date = date;
-	int bank_branch;
+		getline(cin, e.Hire_Date);
+	} while (!Verify_Hire_Date(e.Hire_Date));
+
 	do {
 		cout << "Enter the bank branch of the employee:" << endl;
-		cin >> bank_branch;
-	} while (!Verify_Bank_Branch(bank_branch));
-	e.Bank_Branch = bank_branch;
+		cin >> e.Bank_Branch;
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter numbers only.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+
+	} while (!Verify_Bank_Branch(e.Bank_Branch));
 	employees[EmployeeCount] = e;
 	EmployeeCount++;
 }
@@ -215,23 +234,34 @@ void DeleteEmployee(Employee* employees, int& EmployeeCount) {
 		cout << "There's no existant employees\n";
 		return;
 	}
-	else {
-		int ID,p;
+	int ID, p;
+	do {
 		cout << "Enter the employee's ID to delete: ";
 		cin >> ID;
-		p = FindEmployee(employees, ID, EmployeeCount);
-		if (p!=-1) {
-			for (int j = p; j < EmployeeCount - 1; j++) {
-				employees[j] = employees[j + 1];
-			}
-			EmployeeCount--;
-			cout << "Employee deleted successfully!\n";
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter numbers only.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
 		}
-		else {
-			cout << "Employee not found!\n";
+
+		break;
+
+	} while (!Verify_Id(ID));
+
+	p = FindEmployee(employees, ID, EmployeeCount);
+	if (p != -1) {
+		for (int j = p; j < EmployeeCount - 1; j++) {
+			employees[j] = employees[j + 1];
 		}
-		
+		EmployeeCount--;
+		cout << "Employee deleted successfully!\n";
 	}
+	else {
+		cout << "Employee not found!\n";
+	}
+
 }
 
 
@@ -240,66 +270,102 @@ void ModifyEmployee(Employee* employees, int EmployeeCount) {
 		cout << "There's no existant employees\n";
 		return;
 	}
-	else {
-		int ID, p;
+	int ID, p;
+	do {
 		cout << "Enter the employee's ID to Modify: ";
 		cin >> ID;
-		p = FindEmployee(employees, ID, EmployeeCount);
-		if (p != -1) {
-			int newID;
-			bool ValidID = true;
-			do {
-				cout << "Enter the new ID of the Employee:" << endl;
-				cin >> newID;
-				if (!Verify_Id(newID)) {
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter numbers only.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+
+	} while (!Verify_Id(ID));
+	p = FindEmployee(employees, ID, EmployeeCount);
+	if (p != -1) {
+		int newID;
+		bool ValidID = true;
+
+		do {
+			cout << "Enter the new ID of the Employee:" << endl;
+			cin >> newID;
+
+			if (cin.fail()) {
+				cout << "Invalid input. Please enter numbers only.\n";
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				ValidID = false;
+				continue;
+			}
+
+			if (!Verify_Id(newID)) {
+				ValidID = false;
+			}
+			else {
+				int exist = FindEmployee(employees, newID, EmployeeCount);
+				if (exist != -1 && exist != p) {
+					cout << "This ID is already used by another employee. Try again.\n";
 					ValidID = false;
 				}
 				else {
-					int exist = FindEmployee(employees, newID, EmployeeCount);
-					if (exist != -1 && exist != p) {
-						cout << "This ID is already used by another employee. Try again.\n";
-						ValidID = false;
-					}
-					else {
-						ValidID = true;
-					}
+					ValidID = true;
 				}
-			} while (ValidID==false);
-			employees[p].id = newID;
-			cin.ignore();
-			do {
-				cout << "Enter the new Name of the employee: ";
-				getline(cin, employees[p].Name);
-			} while (!Verify_Name(employees[p].Name));
-			do {
-				cout << "Enter the new Last Name of the employee: ";
-				getline(cin, employees[p].Last_Name);
-			} while (!Verify_Last_Name(employees[p].Last_Name));
-			do {
-				cout << "Enter the new Adress of the employee: ";
-				getline(cin, employees[p].Address);
-			} while (!Verify_Address(employees[p].Address));
-			do {
-				cout << "Enter the Salary of the employee:" << endl;
-				cin >> employees[p].Salary;
-			} while (!Verify_Salary(employees[p].Salary));
-			cin.ignore();
-			do {
-				cout << "Enter the Hire Date of the employee: ";
-				getline(cin, employees[p].Hire_Date);
-			} while (!Verify_Hire_Date(employees[p].Hire_Date));
-			do {
-				cout << "Enter the bank branch of the employee:" << endl;
-				cin >> employees[p].Bank_Branch;
-			} while (!Verify_Bank_Branch(employees[p].Bank_Branch));
-			cout << "Employee modified successfully!\n";
-		}
-		else {
-			cout << "Employee not found!\n";
-			return;
-		}
+			}
+		} while (ValidID == false);
 
+		employees[p].id = newID;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		do {
+			cout << "Enter the new Name of the employee: ";
+			getline(cin, employees[p].Name);
+		} while (!Verify_Name(employees[p].Name));
+		do {
+			cout << "Enter the new Last Name of the employee: ";
+			getline(cin, employees[p].Last_Name);
+		} while (!Verify_Last_Name(employees[p].Last_Name));
+		do {
+			cout << "Enter the new Adress of the employee: ";
+			getline(cin, employees[p].Address);
+		} while (!Verify_Address(employees[p].Address));
+		do {
+			cout << "Enter the Salary of the employee:" << endl;
+			cin >> employees[p].Salary;
+
+			if (cin.fail()) {
+				cout << "Invalid input. Please enter a valid number.\n";
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				continue;
+			}
+
+		} while (!Verify_Salary(employees[p].Salary));
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');		
+		do {
+			cout << "Enter the Hire Date of the employee: ";
+			getline(cin, employees[p].Hire_Date);
+		} while (!Verify_Hire_Date(employees[p].Hire_Date));
+		do {
+			cout << "Enter the bank branch of the employee:" << endl;
+			cin >> employees[p].Bank_Branch;
+
+			if (cin.fail()) {
+				cout << "Invalid input. Please enter numbers only.\n";
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				continue;
+			}
+
+		} while (!Verify_Bank_Branch(employees[p].Bank_Branch));
+		cout << "Employee modified successfully!\n";
 	}
+	else {
+		cout << "Employee not found!\n";
+		return;
+	}
+
+	
 }
 
 
@@ -352,7 +418,7 @@ void DisplayEmployeesGroupedByBankBranch(Employee* employees, int EmployeeCount)
 	int i = 0;
 	while (i < EmployeeCount) {
 		int CurrentBranch = BranchSort[i].Bank_Branch;
-		cout << "\n=========Bank Branch: " << BranchSort[i].Bank_Branch <<"=========\n";
+		cout << "\n=========Bank Branch: " << BranchSort[i].Bank_Branch << "=========\n";
 		while (i < EmployeeCount && BranchSort[i].Bank_Branch == CurrentBranch) {
 			PrintEmployee(BranchSort[i]);
 			i++;
@@ -395,7 +461,7 @@ string Reformulate(string hiredate) {
 	return year + month + day;
 }
 
-void Delete_Closed_Accounts_And_Store_Them(customer*& customers, int& CustomerCount, customer*& archived, int& ArchivedCount, int& ArchivedCapacity){
+void Delete_Closed_Accounts_And_Store_Them(customer*& customers, int& CustomerCount, customer*& archived, int& ArchivedCount, int& ArchivedCapacity) {
 	int i = 0;
 	while (i < CustomerCount) {
 
@@ -433,6 +499,72 @@ void Display_Archived_Accounts(customer* archived, int ArchivedCount) {
 		PrintCustomer(archived[i]);
 	}
 }
+void Manage_Loan_Requests(Queue& Q, customer* customers, int CustomerCount) {
+	if (IsEmpty(Q)) {
+		cout << "No loan requests to manage.\n";
+		return;
+	}
+	int i = 1;
+	while (!IsEmpty(Q)) {
+		loan current_request = FrontElement(Q);
+		int customer_account_number_having_the_loan_request = current_request.Account_Holder_Number;
+		cout << "Processing Loan Request number  " << i << ":\n"
+			<< "Type: " << current_request.Loan_Type << "\n"
+			<< "Principal Amount: " << current_request.Principle_Amount << "\n"
+			<< "Start Date: " << current_request.Start_Date << "\n"
+			<< "End Date: " << current_request.End_Date << "\n";
+		string decision;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		do {
+			cout << "Approve this loan request? (Yes or No): ";
+			getline(cin, decision);
+		} while ((decision != "Yes") && (decision != "No"));
+
+		if (decision == "Yes") {
+			double interest_rate_to_approve = 0.0;
+			string input;
+			while (true) {
+				cout << "Please enter the interest rate of this loan (%) : ";
+				cin >> input;
+
+				// Remove trailing %
+				if (!input.empty() && input.back() == '%')
+					input.pop_back();
+
+				try {
+					interest_rate_to_approve = stod(input);
+				}
+				catch (...) {
+					cout << "Invalid format. Please enter a number like 5 or 5%.\n";
+					continue;
+				}
+
+				if (interest_rate_to_approve < 0 || interest_rate_to_approve > 20) {
+					cout << "The interest rate should be between 0% and 20%.\n";
+					continue;
+				}
+
+				current_request.Interest_Rate = interest_rate_to_approve;
+				int j = 0;
+				while (j < CustomerCount) {
+					if (customers[j].Account_Number == customer_account_number_having_the_loan_request) {
+						insert(customers[j].Loan_List, current_request, customers[j].Loan_List.size + 1);
+						Dequeue(Q);
+						cout << "Loan request approved and added to customer's loan list.\n";
+						return;
+					}
+					j++;
+				}
+			}
+		}
+		else {
+			cout << "Loan request denied and deleted .\n";
+			Dequeue(Q);
+		}
+		i++;
+	}
+}
 
 void Display_List_Of_Loans_For_Specific_Customer(customer* customers, int CustomerCount) {
 	if (CustomerCount == 0) {
@@ -445,8 +577,16 @@ void Display_List_Of_Loans_For_Specific_Customer(customer* customers, int Custom
 		do {
 			cout << "Enter the customer's Account Number to display its loan list: " << endl;
 			cin >> account_number_to_display_its_loan_list;
+			if (cin.fail()) {
+				cout << "Invalid input. Please enter numbers only.\n";
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				ValidAccountNumber = false;
+				continue;
+			}
 			if (!VerifyAccountNumber(account_number_to_display_its_loan_list, customers, CustomerCount)) {
 				ValidAccountNumber = false;
+				continue;
 			}
 			p = FindCustomer(customers, account_number_to_display_its_loan_list, CustomerCount);
 			if ((p == -1) && (VerifyAccountNumber(account_number_to_display_its_loan_list, customers, CustomerCount))) {
@@ -460,6 +600,228 @@ void Display_List_Of_Loans_For_Specific_Customer(customer* customers, int Custom
 		Display_Loan_List(customers[p]);
 	}
 }
+void AddCustomer(customer*& customers, int& CustomerCount, int& Customer_Capacity) {
+	if (CustomerCount == Customer_Capacity) {
+		NewCustomerArray(customers, Customer_Capacity, CustomerCount);
+	}
+	customer c;
+	do {
+		cout << "Enter the account number of the customer:" << endl;
+		cin >> c.Account_Number;
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter numbers only.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	} while (!(VerifyAccountNumber(c.Account_Number, customers, CustomerCount)));
+
+	do {
+		cout << "Enter the IBAN of the customer:" << endl;
+		cin >> c.IBAN;
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter numbers only.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	} while (!VerifyIBAN(c.IBAN));
+
+	do {
+		cout << "Enter the the branch code of the customer:" << endl;
+		cin >> c.Branch_Code;
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter valid input.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	} while (!VerifyBranchCode(c.Branch_Code));
+
+	do {
+		cout << "Enter the Account holder name of the customer:" << endl;
+		getline(cin, c.Account_Holder_Name);
+	} while (!VerifyAccountHolderName(c.Account_Holder_Name));
+
+	do {
+		cout << "Enter the Opening date of the customer account:" << endl;
+		getline(cin, c.Opening_date);
+	} while (!VerifyOpeningDate(c.Opening_date));
+
+	do {
+		cout << "Enter the status of the customer account:" << endl;
+		getline(cin, c.Status);
+	} while (!VerifyStatus(c.Status));
+
+	do {
+		cout << "Enter the Balance of the customer account:" << endl;
+		cin >> c.Balance;
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter a valid number.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+
+	} while (!VerifyBalance(c.Balance));
+
+	do {
+		cout << "Enter the PIN of the customer:" << endl;
+		cin >> c.PIN;
+
+		if (cin.fail()) {
+			cout << "Invalid input. Please enter numbers only.\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			continue;
+		}
+
+	} while (!VerifyPIN(c.PIN));
+
+	c.Loan_List = create_List();
+	c.Transaction_Stack.Top = 0;
+	customers[CustomerCount] = c;
+	CustomerCount++;
+}
+
+
+void ChangeCustomerAccountStatus(customer* customers, int CustomerCount) {
+	if (CustomerCount == 0) {
+		cout << "There are no existing customers.\n";
+		return;
+	}
+
+	int accountNumber;
+	int index = -1;
+
+
+	do {
+		cout << "Enter the customer's Account Number to change its status: ";
+		cin >> accountNumber;
+
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input. Please enter a numeric 6-digit account number.\n";
+			continue;
+		}
+
+
+		if (!VerifyExistingAccountNumber(accountNumber, customers, CustomerCount)) {
+			cout << "Account number not found or invalid. Please enter a valid existing 6-digit account number.\n";
+			continue;
+		}
+
+		index = FindCustomer(customers, accountNumber, CustomerCount);
+		if (index == -1) {
+			cout << "Customer not found. Please make sure of the Account Number.\n";
+		}
+
+	} while (index == -1);
+
+
+	string newStatus;
+	do {
+		cout << "Enter the new account status ('Active', 'Inactive', or 'Closed'): ";
+		cin >> newStatus;
+
+		if (!VerifyStatus(newStatus)) {
+			cout << "Invalid status. Please enter 'Active', 'Inactive', or 'Closed'.\n";
+		}
+
+	} while (!VerifyStatus(newStatus));
+
+
+	customers[index].Status = newStatus;
+	cout << "Customer account status changed successfully!\n";
+}
+
+void Change_Status_Of_A_Loan(customer* customers, int CustomerCount) {
+	int loanID;
+	do {
+		cout << "Enter a loan ID: ";
+		cin >> loanID;
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input. Please enter a numeric Loan ID.\n";
+			continue;
+		}
+
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		if (!VerifyLoanID(loanID)) {
+			cout << "Invalid Loan ID format. Please enter a valid 6-digit Loan ID.\n";
+		}
+
+	} while (!VerifyLoanID(loanID));
+
+	int i = 0;
+	while (i < CustomerCount) {
+		node* current = customers[i].Loan_List.head;
+		while (current) {
+			if (current->data.Loan_ID == loanID) {
+				string newStatus;
+				do {
+					cout << "Enter the new loan status: " << endl;
+					cin >> newStatus;
+				} while (!VerifyLoanStatus(newStatus));
+				current->data.Loan_Status = newStatus;
+				cout << "Loan status changed successfuly" << endl;
+				return;
+			}
+			current = current->next;
+		}
+		i++;
+	}
+	cout << "Loan with ID " << loanID << " not found." << endl;
+}
+
+
+void Move_Completed_Loans_for_a_single_customer(customer& c, CompletedLoanList* completed_loans) {
+
+	node* current = c.Loan_List.head;
+	int pos = 1;
+
+	while (current) {
+		node* nextNode = current->next;
+
+		if (current->data.Loan_Status == "Completed") {
+
+			Insert_Completed_Loan(completed_loans, current->data, completed_loans->size + 1);
+			removeAt(&c.Loan_List, pos);
+		}
+		else {
+			pos++;
+		}
+		current = nextNode;
+
+	}
+}
+
+void Move_All_Completed_Loans(customer* customers, int CustomerCount, CompletedLoanList* CompletedLoansList) {
+	for (int i = 0; i < CustomerCount; i++) {
+		Move_Completed_Loans_for_a_single_customer(customers[i], CompletedLoansList);
+	}
+	cout << "Completed loans moved from all customers.\n";
+}
+
+
 void Finalize_Day(customer* customers, int CustomerCount, DailyTransactionsLogList* DailyLogList) {
 	string current_date = GetTodayDate();
 	string choice;
@@ -488,7 +850,7 @@ void Finalize_Day(customer* customers, int CustomerCount, DailyTransactionsLogLi
 //-----------------------------------------------INTERFACE FUNTION--------------------------------------------------
 
 
-void employee_interface(Employee*& employees, int& EmployeeCount, int& capacity,customer*& customers, int& CustomerCount, int& Customer_Capacity, customer*& archived, int& ArchivedCount, int& Archived_Capacity, CompletedLoanList& CompletedLoansList, Queue* Q, DailyTransactionsLogList* Daily_Transactions_Log_List)
+void employee_interface(Employee*& employees, int& EmployeeCount, int& capacity, customer*& customers, int& CustomerCount, int& Customer_Capacity, customer*& archived, int& ArchivedCount, int& Archived_Capacity, CompletedLoanList& CompletedLoansList, Queue* Q, DailyTransactionsLogList* Daily_Transactions_Log_List)
 {
 	int employee_choice;
 
@@ -591,5 +953,4 @@ void employee_interface(Employee*& employees, int& EmployeeCount, int& capacity,
 	} while (employee_choice != 0);
 
 }
-
 
